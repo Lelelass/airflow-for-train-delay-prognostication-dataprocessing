2bfe3e8317d252fa738d5e914cf6ad941f5bd58f
Lelelass <82147263+Lelelass@users.noreply.github.com>
2023-06-28 08:56:26 +0200

    starting with azure authentification start group in setup

--- dags/static_data_retrieval.py
+++ dags/static_data_retrieval.py
@@ -1,25 +1,11 @@
 from datetime import datetime
 from airflow.decorators import dag, task_group, task
-from include.setup import setup_directories
-from include.rt_static_gtfs.extract import extract_queue_time
-from include.rt_static_gtfs.load import load_datalake
+from include.setup import connect_to_storage
 
-@dag(dag_id = "queue_time_ELT", start_date=datetime(2023,6,8), end_date=datetime(2023,6,11), catchup=False)
-def queue_time_ELT():
-    setup = setup_directories()
-    extract_queue_time_ = extract_queue_time()
-    load_queue_time = load_datalake()
+@dag(dag_id = "static_data_retrieval", start_date=datetime(2023,6,28), end_date=datetime(2023,6,29), catchup=False)
+def static_data_retrieval():
+    setup = connect_to_storage()
 
+    setup
 
-    # dummy parrallel task group
-    @task_group
-    def airquality_extract():
-        @task(task_id="extract")
-        def extract():
-            return "temperature dummy"
-        
-    setup >> extract_queue_time_ >> load_queue_time
-
-    setup >> airquality_extract()
-
-queue_time_ELT()
+static_data_retrieval()
--- include/setup.py
+++ include/setup.py
@@ -1,11 +1,31 @@
+# %%
+
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from pathlib import Path
 from airflow.decorators import task_group
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 
 data_path = Path(__file__).parents[1] / "data"
 datalake_path = data_path / "data_lake"
 data_warehouse_path = data_path / "data_warehouse"
 
+def _connect_to_storage():
+    token_credential = DefaultAzureCredential()
+
+    blob_service_client = BlobServiceClient(
+            account_url="https://trainmetricsll.blob.core.windows.net",
+            credential=token_credential)
+    
+    return blob_service_client
+
+blob_service_client = _connect_to_storage()
+list(blob_service_client.list_containers())
+
+
+
+# %%
 
 @task_group(group_id="setup_data_directories")
 def setup_directories():
@@ -20,3 +40,19 @@ def setup_directories():
     )
 
     create_directories >> success_setup
+
+
+@task_group(group_id="connect_to_azure_storage")
+def connect_to_storage():
+    establish_connection = PythonOperator(
+        task_id = "establish_connection",
+        python_callable= _connect_to_storage
+    )
+
+    success_setup = BashOperator(
+    task_id =  "setup_success",
+    bash_command = f"echo setup data directory successful"
+    )
+
+    establish_connection >> success_setup
+# %%
