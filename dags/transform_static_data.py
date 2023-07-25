3e721bc806789ee0db9dfc5610e00c7181f1e41b
Lelelass <82147263+Lelelass@users.noreply.github.com>
2023-07-25 10:52:51 +0200

    update dags

--- dags/transform_static_data.py
+++ dags/transform_static_data.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from airflow.decorators import dag
+from include.rt_static_gtfs.transform_static import transform_static_data
 
-@dag(dag_id = "static_data_transformation", start_date=datetime(2023,7,24), end_date=datetime(2023,7,25), catchup=False)
+@dag(dag_id = "static_data_transformation", start_date=datetime(2023,7,24), end_date=datetime(2023,7,26), catchup=False)
 def static_data_transformation():
-    pass
+    transform_static_data_from_blob = transform_static_data()
+
+    transform_static_data_from_blob
 
 static_data_transformation()
