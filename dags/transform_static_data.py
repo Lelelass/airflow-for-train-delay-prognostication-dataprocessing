efc8074230b361140bb3f4cf3bce190162e9847b
Ubuntu <azureuser@ubuntu-machine.n0przv5d2xiuvae0vtgun2qhpc.fx.internal.cloudapp.net>
2023-07-26 07:33:48 +0000

    scheduler set in cron fromat

--- dags/transform_static_data.py
+++ dags/transform_static_data.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from airflow.decorators import dag
 from include.rt_static_gtfs.transform_static import transform_static_data
 
-@dag(dag_id = "static_data_transformation", start_date=datetime(2023,7,26), schedule_interval='0 6 * * *', catchup=True)
+@dag(dag_id = "static_data_transformation", start_date=datetime(2023,7,26), schedule_interval='0 6 * * *', catchup=False)
 def static_data_transformation():
     transform_static_data_from_blob = transform_static_data()
 
