507fa2451c692d2be0ca9d9185b4a8ee125bd438
Ubuntu <azureuser@ubuntu-machine.n0przv5d2xiuvae0vtgun2qhpc.fx.internal.cloudapp.net>
2023-07-28 12:44:50 +0000

    functioning _retrieve_static_from_blob to _train_trips_at_day_to_csv

--- include/rt_static_gtfs/transform_static.py
+++ include/rt_static_gtfs/transform_static.py
@@ -5,14 +5,14 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import datetime as dt
+import pandas as pd
 
 temp_data_path = Path("./data/static_data_transform_temp")
 today = dt.datetime.now().strftime("%Y-%m-%d")
 
 
-def _insure_data_availability(list_of_blobs:list)->bool:
+def _insure_data_availability(list_of_blobs:list)->tuple:
     """A check function for insuring that a static file exist from the same day of execution. Returns a bool at first tuple entry for the outcome of _retrieve_static_from_blob execution. Returns a Filename as the second tuple entry if the validity of data in insured """
-    import datetime as dt
     import re
     today = dt.datetime.now().strftime("%Y-%m-%d")
     if len((matches:=[blob_name for blob_name in list_of_blobs if re.search(today, blob_name) is not None])) > 0:
@@ -52,40 +52,44 @@ def _retrieve_static_from_blob(account_name:str, container_name:str, shared_acce
             print(f"Reading the daily zip in blob storage failed. Data avilability = {data_availability} - {ce}")
 
 
-def _load_gtfs()->tuple:
-    """ Uses the gtfs functions module to output a raw gtfs static zip into exploitable Pandas DataFrames, routes and stops are returned in a tuple"""
+def _load_gtfs()->None:
+    """ Uses the gtfs functions module to output a raw gtfs static zip into exploitable Pandas DataFrames, routes and stops are pickled in temp folder"""
     from gtfs_functions import Feed
     gtfs_path = list(temp_data_path.rglob("*.zip"))[0]
     feed = Feed(gtfs_path)
     routes = feed.routes
     stops = feed.stop_times
-    return routes, stops
+    routes.to_pickle(temp_data_path.joinpath("routes.pkl"))
+    stops.to_pickle(temp_data_path.joinpath("stops.pkl"))
 
-def _get_train_trips_at_day(routes, stops)->object:
+def _get_train_trips_at_day()->None:
     """Filters train routes from all routes in the GTFS and return a dataframe of the daily train trips"""
     TRAIN_OPERATORS = ('Pågatåg','PågatågExpress','Krösatåg','Öresundståg')
+    routes = pd.read_pickle(temp_data_path.joinpath("routes.pkl"))
+    stops = pd.read_pickle(temp_data_path.joinpath("stops.pkl"))
     trafficked_train_routes_at_day = routes.where(routes.route_desc.isin(TRAIN_OPERATORS)).dropna(how='all')
     train_trips_at_day = stops.where(stops.route_id.isin(trafficked_train_routes_at_day.route_id)).dropna(how = "all")
     train_trips_at_day.drop(["pattern","timepoint", "geometry", "shape_id", "parent_station", "location_type", "service_id"], axis=1, inplace=True)
-    return train_trips_at_day
+    train_trips_at_day.to_pickle(temp_data_path.joinpath('train_trips_at_day.pkl'))
 
-def _train_trips_at_day_to_csv(train_trips_at_day)->None:
-    import datetime as dt
+def _train_trips_at_day_to_csv()->None:
     import pandas as pd
+    train_trips_at_day = pd.read_pickle(temp_data_path.joinpath('train_trips_at_day.pkl'))
     csv_filepath = temp_data_path.joinpath(f"./static-{today}.csv")
     train_trips_at_day.to_csv(csv_filepath)
-    return csv_filepath
+    return csv_filepath.as_posix() #As posix for xcom pushable path
     
 
-def _store_train_trips_at_day_csv_blob(train_trips_csv_fpath:Path, account_name:str, container_name:str, shared_access_key:str)->None:
+def _store_train_trips_at_day_csv_blob(task_instance, account_name:str, container_name:str, shared_access_key:str)->None:
     """Push the daily train trips to the storage account container as a csv blob"""
     from azure.storage.blob import BlobServiceClient
     account_url=f"https://{account_name}.blob.core.windows.net"
-
+    train_trips_csv_fpath= task_instance.xcom_pull(task_ids ="save_train_trips_at_day_as_csv")
+    print(train_trips_csv_fpath)
     blob_service_client = BlobServiceClient(account_url,credential=shared_access_key)
     container_client = blob_service_client.get_container_client(container_name)
-    blob_client = container_client.get_blob_client(train_trips_csv_fpath.parts[-1])
-    blob_client.upload_blob(train_trips_csv_fpath.as_posix(), overwrite=True)
+    blob_client = container_client.get_blob_client('static-2023-07-28.csv')
+    blob_client.upload_blob(train_trips_csv_fpath, overwrite=True)
 
 
 
@@ -101,27 +105,23 @@ def transform_static_data():
         python_callable=_retrieve_static_from_blob,
         op_kwargs = {"account_name" : account_name,
                      "shared_access_key": shared_access_key,
-                     "container_name":"gtfs-static"},
-        do_xcom_push = False
+                     "container_name":"gtfs-static"}
     )
 
     load_gtfs = PythonOperator(
         task_id ="load_static_regional_gtfs",
         python_callable=_load_gtfs,
-        do_xcom_push = True
     )
 
     get_train_trips_at_day = PythonOperator(
         task_id ="get_train_trips_at_day",
         python_callable= _get_train_trips_at_day,
-        op_args=[],
-        do_xcom_push=True
     )
 
     train_trips_at_day_to_csv = PythonOperator(
         task_id = "save_train_trips_at_day_as_csv",
         python_callable=_train_trips_at_day_to_csv,
-        op_args=[]
+        do_xcom_push = True
     )
 
     store_train_trips_at_day_csv_blob = PythonOperator(
@@ -132,7 +132,11 @@ def transform_static_data():
                 "container_name":"gtfs-static-csvs"}
     )
 
-    clear_temp_data = BashOperator()
+    clear_temp_data = BashOperator(
+        task_id =  "remove_static_local_files",
+        # Find and delete files staring with skane and ending by .zip as well as csv
+        bash_command = f"cd {temp_data_path.as_posix()} && rm $(ls | grep -E 'skane.*.zip|.csv|.pkl')"
+    )
 
 
     retrieve_static_from_blob >> load_gtfs >> get_train_trips_at_day >> train_trips_at_day_to_csv>> store_train_trips_at_day_csv_blob >> clear_temp_data
