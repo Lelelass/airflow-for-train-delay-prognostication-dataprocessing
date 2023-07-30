96d4a666bdeab3a47db763329ae06af78f455796
Ubuntu <azureuser@ubuntu-machine.n0przv5d2xiuvae0vtgun2qhpc.fx.internal.cloudapp.net>
2023-07-30 14:07:50 +0000

    correct csv upload to blob static transformed

--- include/rt_static_gtfs/transform_static.py
+++ include/rt_static_gtfs/transform_static.py
@@ -88,7 +88,7 @@ def _store_train_trips_at_day_csv_blob(task_instance, account_name:str, containe
     print(train_trips_csv_fpath)
     blob_service_client = BlobServiceClient(account_url,credential=shared_access_key)
     container_client = blob_service_client.get_container_client(container_name)
-    blob_client = container_client.get_blob_client('static-2023-07-28.csv')
+    blob_client = container_client.get_blob_client(f'static-{today}.csv')
     blob_client.upload_blob(train_trips_csv_fpath, overwrite=True)
 
 
