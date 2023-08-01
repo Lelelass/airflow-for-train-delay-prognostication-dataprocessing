d6fa9950f3d1df0efabcef09ca85f652e6bbd7c1
Ubuntu <azureuser@ubuntu-machine.n0przv5d2xiuvae0vtgun2qhpc.fx.internal.cloudapp.net>
2023-08-01 06:49:07 +0000

    dag for realtime data retrieval

---
+++ dags/realtime_data_retrieval.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from airflow.decorators import dag
+from include.rt_gtfs.extract_rt import upload_rt_pb_data_to_azure_blob_storage
+
+
+@dag(dag_id = "realtime_data_retrieval", start_date=datetime(2023,8,1), schedule_interval='*/5 5-9 * * *', catchup=False)
+def realtime_data_retrieval():
+    upload_realtime_data_to_azure =upload_rt_pb_data_to_azure_blob_storage()
+
+    upload_realtime_data_to_azure
+
+realtime_data_retrieval()
