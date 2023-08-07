7aa037c8f84bdc2375a68fbe796712bd0fbc19d5
Lelelass <82147263+Lelelass@users.noreply.github.com>
2023-08-07 19:06:14 +0200

    cron expression correct from halfday to day collection

--- dags/realtime_data_retrieval.py
+++ dags/realtime_data_retrieval.py
@@ -3,7 +3,7 @@ from airflow.decorators import dag
 from include.rt_gtfs.extract_rt import upload_rt_pb_data_to_azure_blob_storage
 
 
-@dag(dag_id = "realtime_data_retrieval", start_date=datetime(2023,8,1), schedule_interval='*/5 5-9 * * *', catchup=False)
+@dag(dag_id = "realtime_data_retrieval", start_date=datetime(2023,8,1), schedule_interval='*/5 5-21 * * *', catchup=False)
 def realtime_data_retrieval():
     upload_realtime_data_to_azure =upload_rt_pb_data_to_azure_blob_storage()
 
