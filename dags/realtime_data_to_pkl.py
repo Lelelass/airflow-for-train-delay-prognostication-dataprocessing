15e91e270cfeff0cc43819b2371ffd8a024c8f2a
Ubuntu <azureuser@ubuntu-machine.n0przv5d2xiuvae0vtgun2qhpc.fx.internal.cloudapp.net>
2023-08-09 07:59:48 +0000

    complete dag syntax

--- dags/realtime_data_to_pkl.py
+++ dags/realtime_data_to_pkl.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from airflow.decorators import dag
-
+from include.rt_gtfs.pb_to_pkl import transform_pb_to_pkl
 
 @dag(dag_id = "realtime_data_to_pkl", start_date=datetime(2023,8,9), schedule_interval=None, catchup=False)
 def realtime_data_retrieval():
-    pass
+    transform_pb_to_pkl_and_store = transform_pb_to_pkl()
 
+    transform_pb_to_pkl_and_store
+    
 realtime_data_retrieval()
